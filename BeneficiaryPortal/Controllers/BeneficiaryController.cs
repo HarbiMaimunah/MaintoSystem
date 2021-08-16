841e2853e5a2e7e80a77ffb4b340decfa639f215
Maimunah AL-Harbi <HarbiMaimunah@gmail.com>
2021-08-16 17:18:36 +0300

    edit on login page

--- BeneficiaryPortal/Controllers/BeneficiaryController.cs
+++ BeneficiaryPortal/Controllers/BeneficiaryController.cs
@@ -1,11 +1,10 @@
-﻿using BackOfficePortal.Filters;
-using BeneficiaryPortal.Models;
+﻿using BeneficiaryPortal.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -21,101 +20,14 @@ namespace BeneficiaryPortal.Controllers
     [ServiceFilter(typeof(ResultFilter))]*/
     public class BeneficiaryController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public static string baseUrl = "http://localhost:16982/api/BeneficiaryEntry/";
-
-        public async Task<IActionResult> Signup()
-        {
-            var buildings = await ListBuildings();
-            ViewBag.BuildingsList = new SelectList(buildings, "Id", "Number");
-            return View();
-        }
-
-        public async Task<IActionResult> Register(BeneficiaryRegistration RegisterInfo)
-        {
-            using (var httpClient = new HttpClient())
-            {
-                StringContent stringContent = new StringContent(JsonConvert.SerializeObject(RegisterInfo), Encoding.UTF8, "application/json");
-                using (var response = await httpClient.PostAsync(baseUrl + "Register", stringContent))
-                {
-                    if (!response.IsSuccessStatusCode)
-                    {
-                        string error = await response.Content.ReadAsStringAsync();
-                        TempData["SignupError"] = error;
-                        return RedirectToAction("Signup");
-                    }
-
-                }
-
-                return RedirectToAction("Signin");
-
-            }
-        }
-
-        public IActionResult Signin()
-        {
-            return View();
-        }
-
-        public async Task<IActionResult> Login(Login Login)
-        {
-            using (var httpClient = new HttpClient())
-            {
-                StringContent stringContent = new StringContent(JsonConvert.SerializeObject(Login), Encoding.UTF8, "application/json");
-                using (var response = await httpClient.PostAsync(baseUrl + "Login", stringContent))
-                {
-                    string token = await response.Content.ReadAsStringAsync();
-
-                    if (!response.IsSuccessStatusCode)
-                    {
-                        string error = await response.Content.ReadAsStringAsync();
-                        TempData["LoginError"] = error;
-                        return RedirectToAction("Signin");
-                    }
-
-                    HttpContext.Session.SetString("Token", token);
-
-                    return RedirectToAction("NewTicket", "Beneficiary");
-
-
-                }
-            }
-        }
-
-        public IActionResult SignOut()
-        {
-            HttpContext.Session.Clear();
-            return RedirectToAction("Login", "Beneficiary");
-        }
-
-        [HttpGet]
-        public async Task<List<Building>> ListBuildings()
-        {
-            var url = baseUrl + "ListBuildings";
-            HttpClient client = new HttpClient();
-            string jsonStr = await client.GetStringAsync(url);
-            var res = JsonConvert.DeserializeObject<List<Building>>(jsonStr).ToList();
-            return res;
-        }
-
-        [HttpGet]
-        public JsonResult ListFloors(int buildingID)
-        {
-            var url = baseUrl + "ListFloors/" + buildingID.ToString();
-            
-            
-        }
+        public static string baseUrl = "http://10.6.8.91:44307/api/Beneficiary/";
 
         public IActionResult NewTicket()
         {
             return View();
         }
 
-        /*public async Task<IActionResult> RequestNewTicket(NewTicket ticket)
+        public async Task<IActionResult> RequestNewTicket(NewTicket ticket)
         {
             using (var httpClient = new HttpClient())
             {
@@ -158,6 +70,12 @@ namespace BeneficiaryPortal.Controllers
 
             TempData["NewTicketConfirmation"] = "Your ticket has been sent successfully";
             return RedirectToAction("NewTicket");
-        }*/
+        }
+
+        public IActionResult SignOut()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login", "BeneficiaryEntry");
+        }
     }
 }
---
+++ BeneficiaryPortal/Controllers/BeneficiaryEntryController.cs
@@ -0,0 +1,101 @@
+﻿using BackOfficePortal.Filters;
+using BeneficiaryPortal.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeneficiaryPortal.Controllers
+{
+    /*[ServiceFilter(typeof(AuthorizeFilter))]
+    [ServiceFilter(typeof(ActionFilter))]
+    [ServiceFilter(typeof(ExceptionFilter))]
+    [ServiceFilter(typeof(ResultFilter))]*/
+    public class BeneficiaryEntryController : Controller
+    {
+        public static string baseUrl = "http://10.6.8.91:44307/api/BeneficiaryEntry/";
+
+        public IActionResult Signup()
+        {
+            /*var buildings = await ListBuildings();
+            ViewBag.BuildingsList = new SelectList(buildings, "Id", "Number");*/
+            return View();
+        }
+
+        public async Task<IActionResult> Register(BeneficiaryRegistration RegisterInfo)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                StringContent stringContent = new StringContent(JsonConvert.SerializeObject(RegisterInfo), Encoding.UTF8, "application/json");
+                using (var response = await httpClient.PostAsync(baseUrl + "Register", stringContent))
+                {
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        string error = await response.Content.ReadAsStringAsync();
+                        TempData["SignupError"] = error;
+                        return RedirectToAction("Signup");
+                    }
+
+                }
+
+                return RedirectToAction("Signin");
+
+            }
+        }
+
+        public IActionResult Signin()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Login(Login Login)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                StringContent stringContent = new StringContent(JsonConvert.SerializeObject(Login), Encoding.UTF8, "application/json");
+                using (var response = await httpClient.PostAsync(baseUrl + "Login", stringContent))
+                {
+                    string token = await response.Content.ReadAsStringAsync();
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        string error = await response.Content.ReadAsStringAsync();
+                        TempData["LoginError"] = error;
+                        return RedirectToAction("Signin");
+                    }
+
+                    HttpContext.Session.SetString("Token", token);
+
+                    return RedirectToAction("NewTicket", "Beneficiary");
+
+                }
+            }
+        }
+
+        [HttpGet]
+        public async Task<List<Building>> ListBuildings()
+        {
+            var url = baseUrl + "ListBuildings";
+            HttpClient client = new HttpClient();
+            string jsonStr = await client.GetStringAsync(url);
+            var res = JsonConvert.DeserializeObject<List<Building>>(jsonStr).ToList();
+            return res;
+        }
+
+        /*[HttpGet]
+        public JsonResult ListFloors(int buildingID)
+        {
+            var url = baseUrl + "ListFloors/" + buildingID.ToString();
+            
+            
+        }*/
+    }
+}
