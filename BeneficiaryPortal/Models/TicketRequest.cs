e205bfae389d71e3dfd8022046d06669162656ff
Maimunah AL-Harbi <HarbiMaimunah@gmail.com>
2021-08-18 11:29:14 +0300

    new ticket

---
+++ BeneficiaryPortal/Models/TicketRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeneficiaryPortal.Models
+{
+    public class TicketRequest
+    {
+        [Required(ErrorMessage = "Please enter a description of the problem")]
+        [RegularExpression(@"^[a-zA-z\s]+$", ErrorMessage = "Accepted characters are alphabets and spaces only")] //Alpha and spaces
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please enter the floor number")]
+        public int FloorId { get; set; }
+
+        [Required(ErrorMessage = "Please choose a type of maintenance")]
+        public int MaintenanceTypeID { get; set; }
+
+        [Required(ErrorMessage = "Please enter date and time for the maintenance")]
+        public DateTime Date { get; set; }
+
+#nullable enable
+        [RegularExpression(@"([0-9a-zA-Z\._-]+.(png|PNG|gif|GIF|jp[e]?g|JP[E]?G))", ErrorMessage = "Accepted file type is image only")] //Image file names
+        public string? Picture { get; set; }
+    }
+}
