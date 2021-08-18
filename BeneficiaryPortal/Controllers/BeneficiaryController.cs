c16b9f8363e60432af8aeb06895dd30de9e35b00
Amnah <aminahahmed1999@gmail.com>
2021-08-18 04:06:52 +0300

    password

--- BeneficiaryPortal/Controllers/BeneficiaryController.cs
+++ BeneficiaryPortal/Controllers/BeneficiaryController.cs
@@ -126,6 +126,47 @@ namespace BeneficiaryPortal.Controllers
             return RedirectToAction("NewTicket");
         }
 
+        //-----------------------------------------------------------------------------------------------------------------------------
 
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendEmail(string email)
+        {
+            string response;
+
+            using (HttpClient client = new HttpClient())
+            {
+                var httpResponse = await client.PostAsJsonAsync("http://localhost:16982/api/SystemUser/" + "SendEmail", email);
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    response = await httpResponse.Content.ReadAsStringAsync();
+                }
+            }
+            return View();
+        }
+        //-----------------------------------------------------------------------------------------------------------------------------
+
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> PostPassword(string tempPassword, string newPassword)
+        {
+            string response;
+
+            using (HttpClient client = new HttpClient())
+            {
+                var httpResponse = await client.PostAsJsonAsync("http://localhost:16982/api/SystemUser/" + "ResetPassword", tempPassword + newPassword);
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    response = await httpResponse.Content.ReadAsStringAsync();
+                }
+            }
+            return View();
+        }
     }
 }
