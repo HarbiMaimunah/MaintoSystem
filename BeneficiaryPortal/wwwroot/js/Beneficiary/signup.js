8984e895dd4df9d9ab5f757e98e1950e9b59681a
Nawar <nwaralrwdan@gmail.com>
2021-08-21 10:57:14 +0300

    ز

--- BeneficiaryPortal/wwwroot/js/Beneficiary/signup.js
+++ BeneficiaryPortal/wwwroot/js/Beneficiary/signup.js
@@ -20,14 +20,12 @@
 
 
 function  GetFloor( URL){
-    debugger
     let Id = $('#buildingsList option:selected').val();
     $.ajax({
         type: 'GET',
         url: URL + "?BuildingNumber=" + Id,
         success: function (data) {
             let items = '<option disabled selected>Select Floor</option>';
-            debugger
             $.each(data, function (i, floor) {
                 items += '<option value="' + floor.value + '">' + floor.text + '</option>';
             });
